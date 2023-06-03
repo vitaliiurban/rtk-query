69de6dab31be2fbc0aaea7f796d7dceada8105a2
vitaliiurban <vitalii.urban.ukr@gmail.com>
2023-06-03 17:05:54 +0300

    implemented method PATCH

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import { useGetUsersQuery } from "./api";
+import { useState } from "react";
+import { useGetUsersQuery, useUpdateUserMutation } from "./api";
 
 const App = () => {
   const { data: users, isLoading, isError } = useGetUsersQuery();
-
+  const [updateUser] = useUpdateUserMutation();
+  const [username, setUsername] = useState("");
   if (isLoading) {
     return <div>Loading...</div>;
   }
@@ -10,13 +12,32 @@ const App = () => {
   if (isError) {
     return <div>Error fetching users</div>;
   }
+  const handleUpdateNickname = (user) => {
+    updateUser({ ...user, username: username });
+  };
 
   return (
-    <ul>
+    <div className="container">
       {users.map((user) => (
-        <li key={user.id}>{user.name}</li>
+        <div key={user.id} className="user">
+          <div>{user.id}</div>
+          <div>{user.name}</div>
+          <div>{user.username}</div>
+          <button onClick={() => handleUpdateNickname(user)}>
+            Change nickname
+          </button>
+        </div>
       ))}
-    </ul>
+      <div className="input">
+        <div>Type here new nickname:</div>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="nickname"
+        />
+      </div>
+    </div>
   );
 };
 
--- src/api.js
+++ src/api.js
@@ -3,13 +3,31 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://jsonplaceholder.typicode.com",
+    baseUrl: "http://localhost:3333",
   }),
   endpoints: (builder) => ({
     getUsers: builder.query({
       query: () => "/users",
     }),
+    updateUser: builder.mutation({
+      query: (user) => ({
+        url: `/users/${user.id}`,
+        method: "PATCH",
+        body: user,
+      }),
+      async onQueryStarted({ id }, { dispatch, queryFulfilled }) {
+        const { data } = await queryFulfilled;
+        dispatch(
+          api.util.updateQueryData("getUsers", undefined, (draft) => {
+            const user = draft.find((p) => p.id === id);
+            if (user) {
+              Object.assign(user, data);
+            }
+          })
+        );
+      },
+    }),
   }),
 });
 
-export const { useGetUsersQuery } = api;
+export const { useGetUsersQuery, useUpdateUserMutation } = api;
